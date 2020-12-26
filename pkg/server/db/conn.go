5797b45e29e0d58cd8782d94b4bcda4730a554c1
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-12-26 22:01:07 +0900

    [fix] データベース作成と使用の修正

--- pkg/server/db/conn.go
+++ pkg/server/db/conn.go
@@ -32,39 +32,39 @@ func init() {
 		Conn, err = sql.Open(driverName, fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", user, password, host, database))
 
 		if err != nil {
-			fmt.Printf("open mysql, %s", err)
+			fmt.Printf("open mysql, %s\n", err)
 			return
 		}
 
-		fmt.Printf("open mysql success")
+		fmt.Println("open mysql success")
 
-		_, err = Conn.Exec("CREATE DATABASE IF NOT EXISTS panenga_db")
+		_, err = Conn.Exec("CREATE DATABASE IF NOT EXISTS ?", database)
 		if err != nil {
-			fmt.Printf("create database, %s", err)
+			fmt.Printf("create database, %s\n", err)
 			return
 		}
 
-		_, err = Conn.Exec("USE panenga_db")
+		_, err = Conn.Exec("USE ?", database)
 		if err != nil {
-			fmt.Printf("use database, %s", err)
+			fmt.Printf("use database, %s\n", err)
 			return
 		}
 
 		_, err = Conn.Exec("CREATE TABLE panels(id INTEGER PRIMARY KEY AUTO_INCREMENT, title VARCHAR(64) NOT NULL, panel_image VARCHAR(255) NOT NULL UNIQUE, created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)")
 		if err != nil {
-			fmt.Printf("create table, %s", err)
+			fmt.Printf("create table, %s\n", err)
 			return
 		}
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES (三井アウトレットパーク札幌北広島, https://file24-d.kuku.lu/files/20201226-0812_c12fae45310a831a7a39834c9ecd9dcb.jpg)")
 		if err != nil {
-			fmt.Printf("insert test data 1, %s", err)
+			fmt.Printf("insert test data 1, %s\n", err)
 			return
 		}
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES (青森ねぶた祭り, https://file24-d.kuku.lu/files/20201226-0814_1e834f3d94c0baf3e5e914dd7135181e.jpg)")
 		if err != nil {
-			fmt.Printf("insert test data 2, %s", err)
+			fmt.Printf("insert test data 2, %s\n", err)
 			return
 		}
 
@@ -72,7 +72,7 @@ func init() {
 		
 	} else {
 		Conn, err = sql.Open(driverName,
-			fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database))
+			fmt.Sprintf("%s:%s@tcp(%s:%s)/%s\n", user, password, host, port, database))
 	}
 	if err != nil {
 		fmt.Printf("open mysql, %s", err)
