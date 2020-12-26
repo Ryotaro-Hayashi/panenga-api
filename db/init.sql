4f0693c05a71f1658ca5abfe0e77ab5f019e5633
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-12-26 18:28:11 +0900

    [add] 暫定でymlファイルを追加

--- db/init.sql
+++ db/init.sql
@@ -10,4 +10,6 @@ CREATE TABLE panels(
   updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO panels (title, panel_image) VALUES ("テストデータ", "https://unit42.paloaltonetworks.com/wp-content/uploads/2019/07/golang-hacker.jpg");
+INSERT INTO panels (title, panel_image) VALUES ("三井アウトレットパーク札幌北広島", "https://file24-d.kuku.lu/files/20201226-0812_c12fae45310a831a7a39834c9ecd9dcb.jpg");
+
+INSERT INTO panels (title, panel_image) VALUES ("青森ねぶた祭り", "https://file24-d.kuku.lu/files/20201226-0814_1e834f3d94c0baf3e5e914dd7135181e.jpg");
