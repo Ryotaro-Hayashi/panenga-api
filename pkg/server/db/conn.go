23d667866b8f6c9c59179592ce8e9593ef15a085
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-12-27 03:28:04 +0900

    [clean] 不要なreturnを削除

--- pkg/server/db/conn.go
+++ pkg/server/db/conn.go
@@ -35,7 +35,6 @@ func init() {
 
 		if err != nil {
 			fmt.Printf("open mysql, %s\n", err)
-			return
 		}
 
 		fmt.Println("* ===== open mysql success ===== *")
@@ -43,63 +42,54 @@ func init() {
 		_, err = Conn.Exec("CREATE DATABASE IF NOT EXISTS heroku_292c3038acfa34f")
 		if err != nil {
 			fmt.Printf("create database, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== create database success ===== *")
 
 		_, err = Conn.Exec("USE heroku_292c3038acfa34f")
 		if err != nil {
 			fmt.Printf("use database, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== use database success ===== *")
 
 		_, err = Conn.Exec("CREATE TABLE panels(id INTEGER PRIMARY KEY AUTO_INCREMENT, title VARCHAR(64) NOT NULL, panel_image VARCHAR(255) NOT NULL UNIQUE, created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
 		if err != nil {
 			fmt.Printf("create table, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== create table success ===== *")
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES ('三井アウトレットパーク札幌北広島', 'https://file24-d.kuku.lu/files/20201226-0812_c12fae45310a831a7a39834c9ecd9dcb.jpg')")
 		if err != nil {
 			fmt.Printf("insert test data 1, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== insert test data 1 success ===== *")
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES ('青森ねぶた祭り', 'https://file24-d.kuku.lu/files/20201226-0814_1e834f3d94c0baf3e5e914dd7135181e.jpg')")
 		if err != nil {
 			fmt.Printf("insert test data 2, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== insert test data 2 success ===== *")
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES ('函館湯川温泉', 'https://file14-d.kuku.lu/files/20201226-1653_dccf804db713b5a6ee66256fdcffb68b.jpg')")
 		if err != nil {
 			fmt.Printf("insert test data 3, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== insert test data 3 success ===== *")
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES ('函館五稜郭タワー', 'https://file14-d.kuku.lu/files/20201226-1651_537299245553a7d06578ebe54fa18b59.png')")
 		if err != nil {
 			fmt.Printf("insert test data 4, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== insert test data 4 success ===== *")
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES ('北海道新幹線・新函館北斗駅', 'https://file14-d.kuku.lu/files/20201226-1651_988537a094b4e60d128b7ecc64ec7d70.jpg')")
 		if err != nil {
 			fmt.Printf("insert test data 5, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== insert test data 5 success ===== *")
 
 		_, err = Conn.Exec("INSERT INTO panels (title, panel_image) VALUES ('福岡のどこか', 'https://file14-d.kuku.lu/files/20201226-1650_0f3624c43d357b05a98dc7529db909d7.jpeg')")
 		if err != nil {
 			fmt.Printf("insert test data 6, %s\n", err)
-			return
 		}
 		fmt.Println("* ===== insert test data 6 success ===== *")
 
